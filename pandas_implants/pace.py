ff7bf8fcd05119ec764e18bfdceb832f6153a454
Jan Pipek <jan.pipek@gmail.com>
2019-04-14 12:56:08 +0200

    PaceArray constructor works

--- pandas_implants/pace.py
+++ pandas_implants/pace.py
@@ -38,7 +38,7 @@ class Pace:
 @register_extension_dtype
 class PaceType(ExtensionDtype):
     name = "pace"
-    type = np.float
+    type = float
     kind = "f"
     na_value = np.nan
 
@@ -51,46 +51,42 @@ class PaceType(ExtensionDtype):
                             "'{}'".format(cls, string))
 
     @classmethod
-    def construct_array_type(cls) -> type:
+    def construct_array_type(cls) -> 'type':
         return PaceArray
 
 
 class PaceArray(ExtensionArray):
-    _dtype = PaceType
+    _can_hold_na = True
 
-    # def __init__(self, values):
-    #     pass
-    def dtype(self):
+    def __init__(self, array, dtype=None, copy: bool = True):
+        array = np.array(array) if copy else np.asarray(array)
+        dtype = dtype if dtype else PaceType()
+
+        if array.dtype.kind in ['f', 'i']:
+            array = array.astype(dtype.type)
+        elif array.dtype.kind in ['U', 'S']:
+            array = np.array([Pace.parse(val) for val in array], dtype=dtype.type)
+        else:
+            raise ValueError(f"Cannot create PaceArray from values of type '{dtype}'.")
+
+        self._dtype = dtype
+        self.data = array
+
+    def dtype(self) -> ExtensionDtype:
         return self._dtype
 
-    def __len__(self):
-        return len(self._data)
+    def __array__(self) -> np.ndarray:
+        return self.data
+
+    def __len__(self) -> int:
+        return len(self.data)
+
+    def __getitem__(self, item):
+        return self.data[item]
 
     @classmethod
-    def _from_sequence(cls, scalars, dtype=None, copy=False):
-        result = PaceArray()
-        data = np.array(
-            [cls.parse_single(scalar) for scalar in scalars],
-            dtype=np.float64
-        )
-        result._dtype = dtype
-        result._data = data
+    def _from_sequence(cls, scalars, dtype=None, copy=False) -> "PaceArray":
+        result = cls(scalars)
         return result
 
-    @classmethod
-    def parse_single(cls, s) -> float:
-        if not s:
-            return None
-        elif s in ["-", "nan", np.nan]:
-            return None
-        else:
-            try:
-                frags = s.split(":")
-                frags = [float(frag) for frag in frags]
-                if len(frags) == 1:
-                    interval = timedelta(seconds=float(frags[0]))
-                else:
-                    interval = timedelta(hours=frags[-3] if len(frags) == 3 else 0, minutes=frags[-2], seconds=frags[-1])
-                return interval.total_seconds()
-            except ValueError:
-                raise ValueError("Cannot parse as time: {0}".format(s))  
+
--- tests/test_pace.py
+++ tests/test_pace.py
@@ -1,9 +1,36 @@
 import numpy as np
 import pandas as pd
+import pytest
 
 import pandas_implants.pace
+from pandas_implants.pace import PaceArray
+from pandas.tests.extension import base
 
 
-def test_pace_array():
+@pytest.fixture
+def data():
+    return PaceArray(["1:00", "2:00"] + 98 * ["3:00"])
+
+
+# https://pandas.pydata.org/pandas-docs/stable/development/extending.html#testing-extension-arrays
+#class TestConstructors(base.BaseConstructorsTests):
+#    pass
+
+
+class TestPaceArrayConstructor():
+    def test_with_strings(self):
+        array = PaceArray(["4:00", "4:20"])
+        assert np.allclose(array, [240, 260])
+
+    def test_with_ints(self):
+        array = PaceArray([240, 260])
+        assert np.allclose(array, [240, 260])
+
+    def test_with_floats(self):
+        array = PaceArray([240.1, 260.5])
+        assert np.allclose(array, [240.1, 260.5])
+
+
+def test_create_series_by_name():
     series = pd.Series(["4:00", "4:20"], dtype="pace")
-    assert np.allclose(series.values, [240, 260])
+    assert np.allclose(np.array(series), [240, 260])
