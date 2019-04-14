448f96ead0d49f042aa2d87138f79514f1f10378
Jan Pipek <jan.pipek@gmail.com>
2019-04-14 12:55:39 +0200

    Pace class

--- pandas_implants/pace.py
+++ pandas_implants/pace.py
@@ -1,9 +1,38 @@
-"""Simple pandas extension dtype + array representing the running
-pace expressed as minutes per km."""
+"""Pandas extension types representing running pace expressed as minutes per km."""
 from datetime import timedelta
 
 import numpy as np
 from pandas.api.extensions import ExtensionDtype, ExtensionArray, register_extension_dtype
+
+
+class Pace:
+    @classmethod
+    def parse(cls, s: str) -> float:
+        if not s:
+            return np.nan
+        elif s in ["-", "nan", np.nan]:
+            return np.nan
+        else:
+            try:
+                frags = s.split(":")
+                frags = [float(frag) for frag in frags]
+                if len(frags) == 1:
+                    interval = timedelta(seconds=float(frags[0]))
+                else:
+                    interval = timedelta(hours=frags[-3] if len(frags) == 3 else 0, minutes=frags[-2], seconds=frags[-1])
+                return interval.total_seconds()
+            except ValueError:
+                raise ValueError("Cannot parse as time: {0}".format(s))
+
+    @classmethod
+    def format(cls, f: float) -> str:
+        return "X:XX"
+
+    def __init__(self, value):
+        if isinstance(value, str):
+            self.value = self.parse(value)
+        else:
+            self.value = float(value)
 
 
 @register_extension_dtype
