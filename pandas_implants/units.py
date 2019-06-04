774484a8b95d6a0c778c4719f0a31d20f7a4980a
Jan Pipek <jan.pipek@showmax.com>
2019-06-04 20:41:29 +0200

    Units...

--- pandas_implants/units.py
+++ pandas_implants/units.py
@@ -1,4 +1,5 @@
 import builtins
+import operator
 import re
 from typing import Union
 
@@ -8,8 +9,13 @@ from numpy.core._multiarray_umath import ndarray
 from pandas.api.extensions import (
     ExtensionDtype,
     ExtensionArray,
+    ExtensionScalarOpsMixin,
     register_extension_dtype,
 )
+from pandas.compat import set_function_name
+from pandas.core.dtypes.inference import is_list_like
+from pandas.core import ops
+
 
 
 @register_extension_dtype
@@ -30,7 +36,7 @@ class UnitsDtype(ExtensionDtype):
 
     @classmethod
     def construct_from_string(cls, string) -> "UnitsDtype":
-        match = re.match(f"{cls.BASE_NAME}\\[(?P<name>\\w+)\\]", string)
+        match = re.match(f"{cls.BASE_NAME}\\[(?P<name>.+)\\]", string)
         if not match:
             raise TypeError(f"Invalid UnitsDtype string: {string}")
         return cls(match["name"])
@@ -41,13 +47,13 @@ class UnitsDtype(ExtensionDtype):
 
     @property
     def name(self) -> str:
-        return f"{self.BASE_NAME}[{self.unit.name}]"
+        return f"{self.BASE_NAME}[{self.unit.to_string()}]"
 
     def __repr__(self):
         return f"{self.__class__.__name__}(\"{self.unit.name}\")"
 
 
-class UnitsExtensionArray(ExtensionArray):
+class UnitsExtensionArray(ExtensionArray, ExtensionScalarOpsMixin):
     def __init__(self, array, unit: Union[None, str, Unit] = None, *, copy: bool = True):
         if isinstance(array, Quantity):
             if copy:
@@ -84,18 +90,41 @@ class UnitsExtensionArray(ExtensionArray):
             result = cls(scalars, copy=copy)
         return result
 
+    @classmethod
+    def _to_quantity(cls, obj) -> Quantity:
+        if isinstance(obj, Quantity):
+            return obj
+        elif isinstance(obj, cls):
+            return Quantity(obj.data, obj.unit)
+        else:
+            return Quantity(obj)
+
     def _formatter(self, boxed=False):
-        return lambda x: f"{x} {self.unit.name}"
+        return lambda x: str(x)
 
     def __getitem__(self, item):
         if np.isscalar(item):
-            return self.data[item] #, unit=self.unit)
+            return Quantity(self.data[item], unit=self.unit)
         else:
             return self.__class__(self.data[item], unit=self.unit)
 
     def isna(self):
         return np.isnan(self.data)
 
+    @classmethod
+    def _create_method(cls, op, coerce_to_dtype=True):
+        def _binop(self, other):
+            self_q = cls._to_quantity(self)
+            other_q = cls._to_quantity(other)
+            result_q = op(self_q, other_q)
+            if coerce_to_dtype:
+                return cls(result_q)
+            else:
+                return result_q
+
+        op_name = ops._get_op_name(op, True)
+        return set_function_name(_binop, op_name, cls)
+    
     # * _from_sequence
     #* _from_factorized
     #* __getitem__
@@ -106,3 +135,7 @@ class UnitsExtensionArray(ExtensionArray):
     #* take
     #* copy
     #* _concat_same_type
+
+
+UnitsExtensionArray._add_arithmetic_ops()
+UnitsExtensionArray._add_comparison_ops()
