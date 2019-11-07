60f2c290f5e8595f6758424a1f79d0c33c47ac8c
Jan Pipek <jan.pipek@gmail.com>
2019-11-07 22:22:44 +0100

    Make all current tests pass

--- tests/test_units.py
+++ tests/test_units.py
@@ -1,35 +1,37 @@
-import pytest
-
 import numpy as np
 import pandas as pd
 import pandas.util.testing as tm
-from astropy.units import Quantity, Unit, m, UnitConversionError
+import pytest
+from astropy.units import m
+from astropy.units import Quantity
+from astropy.units import Unit
+from astropy.units import UnitConversionError
 from pandas.tests.extension import base
 from pandas.tests.extension.base import BaseOpsUtil
+from pandas.tests.extension.conftest import *
 
-from pandas_implants.units import UnitsDtype, UnitsExtensionArray, UnitsSeriesAccessor
+from pandas_implants.units import UnitsDtype
+from pandas_implants.units import UnitsExtensionArray
+from pandas_implants.units import UnitsSeriesAccessor
 
 try:
-    from pandas.conftest import (
-        all_arithmetic_operators,
-        all_compare_operators,
-    )
+    from pandas.conftest import all_arithmetic_operators, all_compare_operators
 except:
-    _all_arithmetic_operators = ['__add__', # '__radd__',
-        '__sub__', # '__rsub__',
-        '__mul__', # '__rmul__',
-        '__floordiv__', #'__rfloordiv__',
-        '__truediv__', #'__rtruediv__',
+    _all_arithmetic_operators = [
+        "__add__",  # '__radd__',
+        "__sub__",  # '__rsub__',
+        "__mul__",  # '__rmul__',
+        "__floordiv__",  #'__rfloordiv__',
+        "__truediv__",  #'__rtruediv__',
         # '__pow__', # '__rpow__',
-        '__mod__', # '__rmod__'
+        "__mod__",  # '__rmod__'
     ]
 
     @pytest.fixture(params=_all_arithmetic_operators)
     def all_arithmetic_operators(request):
         return request.param
 
-    @pytest.fixture(params=['__eq__', '__ne__', '__le__',
-                        '__lt__', '__ge__', '__gt__'])
+    @pytest.fixture(params=["__eq__", "__ne__", "__le__", "__lt__", "__ge__", "__gt__"])
     def all_compare_operators(request):
         return request.param
 
@@ -37,6 +39,11 @@ except:
 @pytest.fixture
 def data():
     return UnitsExtensionArray([1, 2] + 98 * [3], m)
+
+
+@pytest.fixture()
+def data_for_twos():
+    return UnitsExtensionArray([2] * 100, m)
 
 
 @pytest.fixture
@@ -60,12 +67,12 @@ def coercible_data():
     return [Quantity(1, "kg"), Quantity(1, "g")]
 
 
-@pytest.fixture(params=['data', 'data_missing'])
+@pytest.fixture(params=["data", "data_missing"])
 def all_data(request, data, data_missing):
     """Parametrized fixture giving 'data' and 'data_missing'"""
-    if request.param == 'data':
+    if request.param == "data":
         return data
-    elif request.param == 'data_missing':
+    elif request.param == "data_missing":
         return data_missing
 
 
@@ -82,6 +89,7 @@ def na_cmp():
             return np.isnan(y.value)
         else:
             return x == y
+
     return cmp
 
 
@@ -121,14 +129,14 @@ def data_repeated(data):
     def gen(count):
         for _ in range(count):
             yield data
+
     return gen
 
 
 # prod forbidden
 # kurt, skew not implemented
-_all_numeric_reductions = ['sum', 'max', 'min',
-                           'mean', 'std', 'var', 'median',]
-                           #'kurt', 'skew']
+_all_numeric_reductions = ["sum", "max", "min", "mean", "std", "var", "median"]
+#'kurt', 'skew']
 
 
 @pytest.fixture(params=_all_numeric_reductions)
@@ -137,7 +145,7 @@ def all_numeric_reductions(request):
 
 
 # These are not implemented
-_all_boolean_reductions = ['all', 'any']
+_all_boolean_reductions = ["all", "any"]
 
 
 @pytest.fixture(params=_all_boolean_reductions)
@@ -162,7 +170,7 @@ class TestCasting(base.BaseCastingTests):
         expected = pd.Series([300, 400], dtype="unit[cm]")
         self.assert_series_equal(result, expected)
 
-    @pytest.mark.parametrize('generic', [False, True])
+    @pytest.mark.parametrize("generic", [False, True])
     def test_convert_from_object(self, generic):
         s = pd.Series([2 * m, 3 * m])
         dtype = "unit" if generic else "unit[m]"
@@ -182,10 +190,13 @@ class TestCasting(base.BaseCastingTests):
         expected = pd.Series(pd.timedelta_range(0, periods=3, freq="h"))
         self.assert_series_equal(result, expected)
 
-class TestDtype(base.BaseDtypeTests): pass
+
+class TestDtype(base.BaseDtypeTests):
+    pass
 
 
-class TestGroupBy(base.BaseGroupbyTests): pass
+class TestGroupBy(base.BaseGroupbyTests):
+    pass
 
 
 class TestGetitem(base.BaseGetitemTests):
@@ -234,7 +245,7 @@ class TestNumericReduce(base.BaseNumericReduceTests):
     def check_reduce(self, s, op_name, skipna):
         # We must check float values
         result = getattr(s, op_name)(skipna=skipna).value
-        expected = getattr(s.astype('float64'), op_name)(skipna=skipna)
+        expected = getattr(s.astype("float64"), op_name)(skipna=skipna)
         tm.assert_almost_equal(result, expected)
 
     # We include some trusted results on top of pandas' ones
@@ -262,7 +273,7 @@ class TestNumericReduce(base.BaseNumericReduceTests):
         assert np.allclose(pd.Series(data).sem() / m, 0.02227015033536137)
 
     def test_var(self, data):
-        assert np.allclose(pd.Series(data).var() / (m **2),  0.0495959595959596)
+        assert np.allclose(pd.Series(data).var() / (m ** 2), 0.0495959595959596)
 
     def test_unsupported(self, data):
         for method in ["any", "all", "prod"]:
@@ -276,7 +287,7 @@ class TestSetitem(base.BaseSetitemTests):
 
 
 class TestParsing(base.BaseParsingTests):
-    @pytest.mark.parametrize('generic', [False, True])
+    @pytest.mark.parametrize("generic", [False, True])
     def test_series_from_string_list(self, generic):
         source = ["1 m", "2 m"]
         dtype = "unit" if generic else "unit[m]"
@@ -332,7 +343,7 @@ class TestComparisonOps(base.BaseComparisonOpsTests):
         s1 = pd.Series([1000, 2000, 3000], dtype="unit[m]")
         s2 = pd.Series([1, 2, 3], dtype="unit[km]")
         s3 = pd.Series([1, 3, 0], dtype="unit[km]")
-        
+
         assert all(s1 == s2)
 
         result = s1 < s3
@@ -356,12 +367,18 @@ class TestComparisonOps(base.BaseComparisonOpsTests):
         with pytest.raises(TypeError):
             s1 < s2
 
+
 class TestRepr:
     def test_repr(self, simple_data):
-        assert "<UnitsExtensionArray>\n[1.0 m, 2.0 m, 3.0 m]\nLength: 3, dtype: unit[m]" == repr(simple_data)
+        assert (
+            "<UnitsExtensionArray>\n[1.0 m, 2.0 m, 3.0 m]\nLength: 3, dtype: unit[m]"
+            == repr(simple_data)
+        )
 
     def test_series_repr(self, simple_data):
-        assert "0   1.0 m\n1   2.0 m\n2   3.0 m\ndtype: unit[m]" == repr(pd.Series(simple_data))
+        assert "0   1.0 m\n1   2.0 m\n2   3.0 m\ndtype: unit[m]" == repr(
+            pd.Series(simple_data)
+        )
 
 
 class TestUnitsSeriesAccessor(BaseOpsUtil):
@@ -400,13 +417,17 @@ class TestUnitsSeriesAccessor(BaseOpsUtil):
 
 class TestUnitsDataFrameAccessor(BaseOpsUtil):
     def test_df_to_si(self):
-        df = pd.DataFrame({
-            "a": pd.Series([1, 2, 3], dtype="unit[km]"),
-            "b": pd.Series([2, 3, 4], dtype="unit[hour]")
-        })
+        df = pd.DataFrame(
+            {
+                "a": pd.Series([1, 2, 3], dtype="unit[km]"),
+                "b": pd.Series([2, 3, 4], dtype="unit[hour]"),
+            }
+        )
         result = df.units.to_si()
-        expected = pd.DataFrame({
-            "a": pd.Series([1000, 2000, 3000], dtype="unit[m]"),
-            "b": pd.Series([7200, 10800, 14400], dtype="unit[s]")
-        })
+        expected = pd.DataFrame(
+            {
+                "a": pd.Series([1000, 2000, 3000], dtype="unit[m]"),
+                "b": pd.Series([7200, 10800, 14400], dtype="unit[s]"),
+            }
+        )
         self.assert_frame_equal(result, expected)
